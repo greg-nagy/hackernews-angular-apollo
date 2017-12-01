627ce8b83e39198ff3aba6f4ed9741b708c19a8a
puffy <nagy.gergo@3pixel.hu>
2017-12-01 20:14:52 +0100

    basic init * predix change to hn- * apollo config * add initial style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GraphQlModule } from './apollo.config';
+import { LinkItemComponent } from './link-item/link-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GraphQlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
